23ab9e0c28d91801cd0b77ab2b368d7c6bdb3036
Gulya-mur <gulenka10@mail.ru>
2023-10-08 14:14:21 +0300

    Change gc input using Union

--- modules/DNA_fastq_filter.py
+++ modules/DNA_fastq_filter.py
@@ -1,15 +1,14 @@
 
-from typing import Tuple
+from typing import Tuple, Union
 
 def cal_gc_content(dna: str) -> int: 
     """Return percent of GC content"""   
     return dna.count('G') + dna.count('C')/ len(dna) * 100
 
-def gc_bounds(dna: str, value: Tuple[int]) -> str:
+def gc_bounds(dna: str, value: Tuple[Union[int, float]]) -> str:
     """
     Define filter boundatry and return dna if it passed the filtering using cal_gc_content
-    """
-    
+    """   
     if isinstance(value, tuple):
         if value[0] <= cal_gc_content(dna) <= value[1]:
             return dna
