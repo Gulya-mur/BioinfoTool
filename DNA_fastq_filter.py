8487a5083515e0b0daf6fe9cc63a65d37db7dd6e
Gulya-mur <gulenka10@mail.ru>
2023-10-08 10:21:33 +0300

    Add funcs to DNA_fastq_filter.py, add main func in main_functions, add additional info (dict) in bio_info,py

---
+++ DNA_fastq_filter.py
@@ -0,0 +1,41 @@
+
+from typing import Dict, Tuple
+
+def cal_gc_content(dna: str) -> int: 
+    """Return percent of GC content"""   
+    return dna.count('G') + dna.count('C')/ len(dna) * 100
+
+def gc_bounds(dna: str, value: Tuple[int]) -> str:
+    """
+    Define filter boundatry and return dna if it passed the filtering using cal_gc_content
+    """
+    
+    if isinstance(value, tuple):
+        if value[0] <= cal_gc_content(dna) <= value[1]:
+            return dna
+    else:
+        if cal_gc_content(dna) <= value:
+            return dna   
+
+def length_bounds(dna: str, value: Tuple[int]) -> str:
+    """
+    Define filter boundatry and return dna if it passed the filtering by set range
+    """
+    if isinstance(value, tuple):
+        if value[0] <= len(dna) <= value[1]:
+            return dna
+    else:
+        if len(dna) <= value:
+            return dna
+   
+
+def quality_threshold(quality: str, value: int):
+    """
+    Define quality of each nucleotide using ASCII table and Phred-33 scale
+    """
+    for qual in quality:
+        nuc_qual = ord(qual)-33
+        if nuc_qual < 0:
+            break
+    return True
+
--- main_functions.py
+++ main_functions.py
@@ -1,6 +1,7 @@
 from DNATool import *
 from typing import List
 from ProteinTool import *
+from DNA_fastq_filter import *
 
 def run_dna_rna_tools(operation: str, seqs: List[str]):
     if len(seqs) >= 2 and operation == "process_multiple_sequences":
@@ -42,3 +43,13 @@ def process_seqs(option: str, seqs: List[str]):
         return FUNC_LIST_INPUT[option](seqs)
     else:
         raise ValueError("Enter valid operation")
+    
+def read_fastq(seqs: Dict[str, tuple] , gc: Tuple[int], length: Tuple[int], quality: int) -> Dict: 
+    """
+    The main function takes 4 arguments. It checks DNA for compliance with conditions and return new Dict with filtered DNA. 
+    """
+    qualitive_dna = {}
+    for name, turp in seqs.items():       
+        if length_bounds(turp[0],length) and gc_bounds(turp[0], gc) and quality_threshold(turp[1],quality):
+                   qualitive_dna[name] = turp
+    return qualitive_dna
