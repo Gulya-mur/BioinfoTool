9dfd3baeabaf11e8c0fa463f38e1e966a1bbcb72
Gulya-mur <gulenka10@mail.ru>
2023-10-19 18:33:03 +0300

    Fix mistakes in filters. Make bool function

--- modules/DNA_fastq_filter.py
+++ modules/DNA_fastq_filter.py
@@ -5,28 +5,23 @@ def cal_gc_content(dna: str) -> int:
     """Return percent of GC content"""   
     return dna.upper().count('G') + dna.upper().count('C')/ len(dna) * 100
 
-def is_pass_by_gc(dna: str, parameters: Tuple[Union[int, float]]) -> str:
+def is_pass_by_gc(dna: str, parameters: Tuple[Union[int, float]]) -> bool:
     """
     Define filter boundatry and return dna if it passed the filtering using cal_gc_content
     """   
     if isinstance(parameters, tuple):
-        if parameters[0] <= cal_gc_content(dna) <= parameters[1]:
-            return dna
+        return parameters[0] <= cal_gc_content(dna) <= parameters[1]
     else:
-        if cal_gc_content(dna) <= parameters:
-            return dna   
+        return cal_gc_content(dna) <= parameters  
 
-def is_pass_by_length(dna: str, parameters: Tuple[int]) -> str:
+def is_pass_by_length(dna: str, parameters: Tuple[int]) -> bool:
     """
     Define filter boundatry and return dna if it passed the filtering by set range
     """
     if isinstance(parameters, tuple):
-        if parameters[0] <= len(dna) <= parameters[1]:
-            return dna
+        return parameters[0] <= len(dna) <= parameters[1]
     else:
-        if len(dna) <= parameters:
-            return dna
-   
+        return len(dna) <= parameters  
 
 def is_pass_by_quality(quality: str, value: int) -> bool:
     """
